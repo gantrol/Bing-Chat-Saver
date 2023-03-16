b9a5b2c3683d3d5467074885d20f13ff61b003ee
gantrol <gantrol.h@qq.com>
2023-03-17 01:11:03 +0800

    feat: 添加截图导出功能（暂不支持图片）

--- contents/dark.ts
+++ contents/dark.ts
@@ -1,4 +1,6 @@
 import type { PlasmoCSConfig } from "plasmo";
+import { DownloadVisitor } from "~utils/visitor";
+import { domToJpeg, domToPng } from "modern-screenshot";
 
 export const config: PlasmoCSConfig = {
   matches: ["*://yiyan.baidu.com/*"],
@@ -40,3 +42,85 @@ const t = function () {
 
 t();
 
+const newConvButtonGroup = document.querySelector("#root > div > div.N_eugr4S > div > div.x8oakxJk > div > span")
+
+const newConvButton = document.querySelector("#root > div > div.N_eugr4S > div > div.x8oakxJk > div > span > span")
+
+const DEFAULT_WITDH = 0;
+
+class DarkDownloadVisitor {
+  static async forImage(func, type, way = "newTab", width = DEFAULT_WITDH) {
+
+    const options =
+      {
+        backgroundColor: "rgb(217, 230, 249)"
+      };
+
+
+    await (new Promise((resolve) => {
+      setTimeout(resolve, 20);
+    }));
+
+    console.log("start to download");
+    const main = <HTMLElement>document.querySelector("div.dialogueCardListContent > div");
+
+    console.log(main);
+    const dataURL = await func(main, options);
+    console.log(dataURL);
+    if (way === "newTab") {
+      requestAnimationFrame(() => {
+        const binaryData = atob(dataURL.split("base64,")[1]);
+        const data = [];
+        for (let i = 0; i < binaryData.length; i++) {
+          data.push(binaryData.charCodeAt(i));
+        }
+        const blob = new Blob(
+          [new Uint8Array(data)],
+          { type: `image/${type}` });
+        const url = URL.createObjectURL(blob);
+
+        window.open(url, "_blank");
+      });
+    } else {
+      const link = document.createElement("a");
+      link.download = `my-image-name.${type}`;
+      link.href = dataURL;
+      link.click();
+      link.remove();
+      console.log("end");
+    }
+  }
+
+  static forPNG = async (width = DEFAULT_WITDH) => {
+    await this.forImage(domToPng, "png", "download", width);
+  };
+
+  static forJPG = async (width = DEFAULT_WITDH) => {
+    await this.forImage(domToJpeg, "jpeg", "download", width);
+  };
+}
+
+
+const button = document.createElement('button');
+button.innerHTML = 'PNG';
+// set the button style
+button.style.position = 'fixed';
+button.style.bottom = '20px';
+button.style.right = '20px';
+button.style.zIndex = '9999';
+button.style.alignItems = 'center';
+button.style.background = '#efebef';
+button.style.borderRadius = '6px';
+button.style.color = '#4955f5';
+button.style.display = 'flex';
+button.style.fontFamily = 'PingFangSC-Medium';
+button.style.fontSize = '16px';
+button.style.fontWeight = '500';
+button.style.height = '40px';
+button.style.justifyContent = 'center';
+button.style.letterSpacing = '0';
+button.style.lineHeight = '20px';
+button.style.userSelect = 'none';
+button.style.width = '120px';
+button.onclick = () => DarkDownloadVisitor.forPNG();
+document.body.appendChild(button);
